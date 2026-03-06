e4bb6fc0e3427375b22620d2ea8d65b5ca95527f
MUNGA <brian@TOP-MacBook.local>
2026-03-06 10:10:04 +0300

    fix: nodejs runtime for auth

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import Google from 'next-auth/providers/google';
 import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcryptjs';
 
+export const runtime = 'nodejs';
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
